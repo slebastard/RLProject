88f757a6e13d94718bb802c4c352f2cef2426df1
slebastard <lebastardsimon@gmail.com>
2018-01-11 20:21:05 +0100

    Updated ToDo in readme

--- HRL_methods.py
+++ HRL_methods.py
@@ -25,13 +25,13 @@ class Option():
 
 
 class MAXQ():
-	def __init__(self, GridWorld, alpha0=0.25, n_iter=5000, actionsHierarchy=None):
+	def __init__(self, GridWorld, alpha0=0.25, expl0=0.9, n_iter=5000, actionsHierarchy=None, optionSet=None):
 		self.GridWorld = GridWorld
 		self.maxActionID = 0
 
 		# A tree containing actions that are either primitives or options
 		if actionsHierarchy is None:
-			actionsHierarchy = Node('Root', type='root')
+			actionsHierarchy = Node('Root', type='option', option=self.rootOption())
 			actionsHierarchy.n_prim = 0
 			actionsHierarchy.n_opt = 0
 			for action in self.GridWorld.action_names:
@@ -40,19 +40,25 @@ class MAXQ():
 				actionsHierarchy.n_prim += 1
 		self.actions = actionsHierarchy		# check this is a shallow copy
 
+		if optionSet is not None:
+			for option in optionSet:
+				self.addOption(option)
+
 		self.V = np.random.rand(self.actions.n_prim + self.actions.n_opt,self.GridWorld.n_states) 		# value function: a matrix mapping (primActionID, state) to expected value
-		self.C = np.random.rand(self.actions.n_opt, self.GridWorld.n_states, self.actions.n_prim + self.actions.n_opt) 		# completion function: a matrix mapping (optionID, state, primActionID) to completion value
+		self.C = np.random.rand(self.actions.n_opt + 1, self.GridWorld.n_states, self.actions.n_prim + self.actions.n_opt) 		# completion function: a matrix mapping (optionID, state, primActionID) to completion value
 		self.alpha0 = alpha0		# initial learning rate. Harmonic learning function is used here
+		self.expl0 = expl0			# initial exploration rate
 		self.n_iter = n_iter
 		self.unkOptCount = 0
-        
+
 		for iter in range(self.n_iter):
 			initState = self.GridWorld.reset()
 			self.time = 0
-			self.run(self.actions.root, initState)
+			self.run(self.actions, initState)
  
     
 	def addOption(self, option):
+		# NB: right now options can only be added at init. To be generalized
 		optionName = option.name
 		if option.name == 'Unknown Option':
 			self.unkOptCount += 1
@@ -60,45 +66,67 @@ class MAXQ():
 
 		Node(optionName, parent=self.actions, actionID = self.maxActionID, type='option', option=option)
 		self.maxActionID += 1
-
+		self.actions.n_opt += 1
 
 	def evaluate(self, task, state):
 		if task.type == 'primitive':
-			return [self.V[task, state], task]
+			return [self.V[task.actionID, state], task.actionID]
 		else:
 			value = {}
 			for subtask in PreOrderIter(task):
-				[value[subtask],_] = self.evaluate(subtask) + self.C[task, state, subtask]
+				[value[subtask.actionID],_] = self.evaluate(subtask, state) + self.C[task.actionID, state, subtask.actionID
 			greedyAction = max(value.iteritems(), key=op.itemgetter(1))[0]
 			return [self.V[greedyAction, state], greedyAction]
 
 
-	def rate(self, time):
+	def learningRate(self, time):
 		return alpha/float(time)
+
+
+	def explorationRate(self):
+		return expl0/float(self.time)
 
 
 	def run(self, task, state):
 		if task.type == 'primitive':
 			[next_state, reward, absorb] = self.GridWorld.step(state, action)
-			alpha = self.rate(self.time)
-			self.V[task, state] = (1-alpha)*self.V[task, state] + alpha*reward
+			alpha = self.learningRate(self.time)
+			self.V[task.actionID, state] = (1-alpha)*self.V[task.actionID, state] + alpha*reward
 			self.time += 1
 			return 1
 		elif task.type == 'option':
 			count = 0
 			while task.option.log == 'active':
-				action = task.option.policy(self.GridWorld.state2coord(state)) 	# ToDo: generalize policies to probabilistic functions, then replace [] by () here
+				action = task.option.policy(self.GridWorld.state2coord[state])
 				N = self.run(action, state)
 				[next_state, reward, absorb] = self.GridWorld.step(state, action)
 				if absorb:
 					task.option.log = 'quit'
 				[greedyValue, greedyAction] = self.evaluate(task, next_state)
 				alpha = self.rate(self.time)
-				self.C[task, state, action] = (1-alpha)*self.C[task, state, action] + alpha*np.power(self.GridWorld.gamma, N)*greedyValue
+				self.C[task.actionID, state, action] = (1-alpha)*self.C[task.actionID, state, action] + alpha*np.power(self.GridWorld.gamma, N)*greedyValue
 				count = count + N
 				state = next_state
-				task.option.escape(self.GridWorld.state2coord(state))
+				task.option.escape(self.GridWorld.state2coord[state])
 			return count
-		elif task.type == 'root':
-			for subtask in task:
-				
+		else:
+			raise ValueError("Action type should be either 'primitive' or 'option'")
+
+
+	def explorationPolicy(self, state):
+		Q = self.V[:, state] + np.hstack(
+			np.zeros((self.actions.n_prim,1))
+			, self.C[0, state, :]
+		)
+		greedyAction = np.argmax(Q)
+		failed = np.random.rand(1) < self.explorationRate()
+		if failed or np.isnan(greedyAction):
+			return np.random.choice(self.GridWorld.state_actions[state])
+		else:
+			return greedyAction
+
+
+	def rootOption(self):
+		initSet = np.ones((self.GridWorld.n_rows, self.GridWorld.n_cols))
+		quitMap = np.zeros((self.GridWorld.n_rows, self.GridWorld.n_cols))
+		return Option(initSet, self.explorationPolicy, quitMap, name='Root Option')
--- readme.txt
+++ readme.txt
@@ -7,8 +7,8 @@ P0
 	- MAXQ algorithm				... to be tested
 	- 2-rooms and/or 4-rooms tasks
 		- Article from Botvinick et al on Drive: look at their implementation of 4-rooms
-		- Implement
-- Nice presentation of results in Notebook
+		- Implement 				2-rooms OK, Q-learning & value iteration work
+- Nice presentation of results in Notebook 	Q-learning + MAXQ on 2-room is done
 
 P1
 - Add option discovery to the framework
